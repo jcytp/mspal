adedcd68b5d56164bfbc0f2d142bb46b43a6a386
0001_toshiki <bugsyuriken@gmail.com>
2021-06-08 15:51:44 +0900

    create API.js

---
+++ sspa/API.js
@@ -0,0 +1,39 @@
+import page from './page.js'
+
+export class API {
+  constructor(obj) {
+    this.url = obj.url
+    this.options = {}
+    this.options.method = obj.method ? obj.method : 'GET'
+    this.options.mode = 'cors'
+    this.options.cache = 'default'
+    this.options.credentials = 'same-origin'
+    this.params = obj.params ? obj.params : []
+  }
+  async call() {
+    console.log(`api call | url: ${this.url}, params: ${this.params}`)
+    const params = {}
+    let url = this.url
+    for (const elem_id of this.params) {
+      const elem = page.id(elem_id)
+      if (elem) {
+        switch (elem.tagName) {
+          case 'INPUT':
+          case 'SELECT':
+            params[elem_id] = elem.value
+            break
+        }
+      }
+    }
+    switch (this.options.method) {
+      case 'GET':
+        // append params to url
+        break
+      case 'POST':
+      case 'PUT':
+        this.options.body = JSON.stringify(params)
+        break
+    }
+    const response = await fetch(url, this.options)
+  }
+}
