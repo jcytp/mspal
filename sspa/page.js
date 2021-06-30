3f7d3f07ea9aef6ca3f2f238ae917951592fbbc0
0001_toshiki <bugsyuriken@gmail.com>
2021-06-30 10:14:00 +0900

    fix bug: async

--- sspa/page.js
+++ sspa/page.js
@@ -1,13 +1,19 @@
 export class page {
   static components = new Map()
+  static special_elems = new Map([
+    ["__window", window],
+  ])
 
   static id(elem_id) {
+    if (page.special_elems.has(elem_id)) {
+      return page.special_elems.get(elem_id)
+    }
     return document.getElementById(elem_id)
   }
   static class(elem_classname) {
     return Array.from(document.getElementsByClassName(elem_classname))
   }
-  static loadComponent(component_id) {
+  async static loadComponent(component_id) {
     if (page.components.has(component_id)) {
       return page.components.get(component_id)
     } else {
@@ -22,7 +28,7 @@ export class page {
     const routes = new Map()
     // find top component from routes
     const component_id = 'Login'
-    let component = page.loadComponent(component_id)
+    let component = await page.loadComponent(component_id)
     component.lender('sspa-root')
   }
 }
