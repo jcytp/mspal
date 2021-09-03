fc4fbd7939c100341a372ab4c4a841e5ca034a23
0001_toshiki <bugsyuriken@gmail.com>
2021-09-04 08:15:50 +0900

    develop

--- sspa/page.js
+++ sspa/page.js
@@ -1,16 +1,19 @@
-import Handler from "./Handler"
+import Handler from "./Handler.js"
 
 export default class page {
   static domain = document.location.host
   static routes = new Map()
   static components = new Map()
+
+  static root_id = "sspa-root"
   static special_elems = new Map([
     ["__window", window],
     ["__body", document.getElementsByTagName("BODY")[0]],
   ])
+
   static back_handler = new Handler({
-    target: '__window',
-    type: 'popstate',
+    target: "__window",
+    type: "popstate",
     listener: async (ev) => {
       if (page.domain == document.location.host && ev.state.startsWith("sspa|")) {
         top_component_id = ev.state.substr(5)
@@ -25,12 +28,14 @@ export default class page {
     }
     return document.getElementById(elem_id)
   }
+
   static class(elem_classname) {
     return Array.from(document.getElementsByClassName(elem_classname))
   }
+
   static async loadComponents(component_id) {
     if (page.components.has(component_id)) {
-      // return page.components.get(component_id)
+      return page.components.get(component_id)
     } else {
       const module = await import (`../components/${component_id}.js`)
       const component = module ? (module.default ? module.default : null) : null
@@ -42,9 +47,10 @@ export default class page {
       } else {
         console.error(`sspa Error: Components "${component_id}" Not Found.`)
       }
-      // return component
+      return component
     }
   }
+
   static lenderComponents(target_id, component_id) {
     const component = page.components.get(component_id)
     component.lender(target_id)
@@ -52,10 +58,12 @@ export default class page {
       page.lenderComponents(id, cmp_id)
     }
   }
-  static async open(top_component_id) {
-    await page.loadComponents(top_component_id)
-    page.lenderComponents(page.id("sspa-root") ? "sspa-root" : "__body", top_component_id)
+
+  static async open(component_id) {
+    await page.loadComponents(component_id)
+    page.lenderComponents(page.id(root_id) ? root_id : "__body", component_id)
   }
+
   static init() {
     // ToDo: read from params.json
     // ToDo: read from routes.json
@@ -76,6 +84,7 @@ export default class page {
     //   }
     // }
   }
+
   static move(path) {
     // ToDo: find top component from routes
     const top_component_id = "Register"
