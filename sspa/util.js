7bc56bb8ab0b8a9a92066bdedce824be1f95c20f
0001_toshiki <bugsyuriken@gmail.com>
2021-09-04 14:03:45 +0900

    debug

--- sspa/util.js
+++ sspa/util.js
@@ -22,8 +22,10 @@ export default class util {
     const escapeRegex = (not_regex) => {
       not_regex.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1")
     }
-    console.log(`rule.split("*") : ${rule.split("*")}`)
-    console.log(`rule.split("*").map(escapeRegex) : ${rule.split("*").map(escapeRegex)}`)
+    // console.log(`escapeRegex('/login') : ${escapeRegex('/login')}`)
+    console.log(rule.split("*"))
+    console.log(rule.split("*").map(escapeRegex))
+    // console.log(`rule.split("*").map(escapeRegex) : ${rule.split("*").map(escapeRegex)}`)
     rule = "^" + rule.split("*").map(escapeRegex).join(".*") + "$"
     const regex = new RegExp(rule)
     console.log(`str: ${str}`)
