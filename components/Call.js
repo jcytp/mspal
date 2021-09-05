912831be5b58613bcee90005eefdfa75b4b775db
0001_toshiki <bugsyuriken@gmail.com>
2021-09-05 12:35:23 +0900

    dev

--- components/Call.js
+++ components/Call.js
@@ -12,8 +12,8 @@ cmp.setHtml(`
   <main>
     <h2>API Call</h2>
     <p>This page shows a sample API call. Thanks to <a href="https://ghibliapi.herokuapp.com/">Studio Ghibli API</a>.</p>
-    <input type="button" id="btn-ghibli-peple" value="get people of ghibli" />
-    <table id="tbl-ghibli-people" class="data">
+    <input type="button" id="btnGhibliPeple" value="get people of ghibli" />
+    <table id="tblGhibliPeople" class="data">
       <thead>
         <tr>
           <td>name</td>
@@ -36,30 +36,30 @@ cmp.addStyle('main')
 cmp.addStyle('data-table')
 
 /* APIs */
-cmp.addAPI('get-ghibli-people', new API({
+cmp.addAPI('get_ghibli_people', new API({
   url: 'https://ghibliapi.herokuapp.com/people',
 }))
-cmp.addAPI('get-ghibli-film', new API({
+cmp.addAPI('get_ghibli_film', new API({
   url: 'https://ghibliapi.herokuapp.com/films/{film-id}',
   params: ['film-id'],
 }))
 
 /* Event Handlers */
-cmp.addHandler('btn-ghibli-people', new Handler({
-  target: 'btn-ghibli-people',
+cmp.addHandler('btn_ghibli_people', new Handler({
+  target: 'btnGhibliPeople',
   type: 'click',
   listener: async (ev) => {
     ev.preventDefault()
-    const result = await cmp.callAPI('get-ghibli-people')
+    const result = await cmp.callAPI('get_ghibli_people')
     const data = result ? await result.json() : null
     if (data) {
-      const table = document.getElementById('tbl-ghibli-people')
+      const table = document.getElementById('tblGhibliPeople')
       table.innerHTML = '<thead><tr><td>name</td><td>gender</td><td>age</td><td>films</td></tr></thead>'
       for (const entry of data) {
         const films = new Array()
         for (film_url of entry.films) {
           const film_id = film_url.split('/').slice(-1)[0]
-          const film_result = await cmp.callAPI('get-ghibli-film', new Map([['film-id', film_id]]))
+          const film_result = await cmp.callAPI('get_ghibli_film', new Map([['film-id', film_id]]))
           const film_data = film_result ? await film_result.json() : null
           if (film_data) {
             films.push(film_data.title)
--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,7 @@
 import {
   Component,
+  Handler,
+  Page,
 } from "../mspal.js"
 
 const cmp = new Component('Header')
@@ -15,5 +17,31 @@ cmp.setHtml(`
     </ul>
   </nav>
 `)
+
+/* Event Handlers */
+cmp.addHandler('move_to_home', new Handler({
+  target: 'clsMoveToHome',
+  type: 'click',
+  listener: (ev) => {
+    ev.preventDefault()
+    Page.move('/home')
+  },
+}))
+cmp.addHandler('move_to_call', new Handler({
+  target: 'clsMoveToCall',
+  type: 'click',
+  listener: (ev) => {
+    ev.preventDefault()
+    Page.move('/call')
+  },
+}))
+cmp.addHandler('move_to_points', new Handler({
+  target: 'clsMoveToPoints',
+  type: 'click',
+  listener: (ev) => {
+    ev.preventDefault()
+    Page.move('/points')
+  },
+}))
 
 export default cmp
